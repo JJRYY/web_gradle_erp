55ffdc6549af2f4b452e71e59fc7c6182d009c5b
Ryong <kjr2436@gmail.com>
2021-04-07 17:11:08 +0900

    complete TitleDao test

--- Scripts/3.test.sql
+++ Scripts/3.test.sql
@@ -1,3 +1,11 @@
 select * from employee;
 select * from title;
 select * from department;
+
+select titleNo, titleName from title;
+
+select titleNo, titleName from title where titleNo = 1;
+
+insert into title values (6, '인턴');
+update title set titleName = 'intern' where titleNo = 6;
+delete from title where titleNo = 6;
